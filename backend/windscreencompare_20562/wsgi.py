130b2f19828813e71159f41f286896f7d9aa42aa
Crowdbotics <team@crowdbotics.com>
2020-09-22 14:26:24 +0000

    Update models

--- backend/windscreencompare_20562/wsgi.py
+++ backend/windscreencompare_20562/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'windscreencompare_20562.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "windscreencompare_20562.settings")
 
 application = get_wsgi_application()
